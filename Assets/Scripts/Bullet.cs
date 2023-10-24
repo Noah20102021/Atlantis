4eac56074efb0d9b69e8e1f712f14d98118efb7e
ErnstStudio <ludwig.lemberger@gmail.com>
2023-10-24 12:08:57 +0200

    bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] float durationbullet = 2.5f, currentTimeBullet;
+
+    public int damage = 25;
+
+  
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(timeOFBullet());
+        currentTimeBullet = durationbullet;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision2D) {
-        
+    IEnumerator timeOFBullet() 
+    {
+        while(currentTimeBullet >= 0) 
+        {
+            yield return new WaitForSeconds(1f);
+            currentTimeBullet--;
+        }
+        DestroyBullet();
+    }
+
+    public void DestroyBullet() 
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        //Enemy enemy = collision2D.collider.GetComponent<Enemy>();
+        if(collision2D.collider.CompareTag("Enemy")) 
+        {
+            //enemy.takedamage(damage);
+            Destroy(gameObject);
+        }
     }
 }
