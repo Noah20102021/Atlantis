7af8457aca654667d3cb7a022f32aaf6a922941c
ErnstStudio <ludwig.lemberger@gmail.com>
2023-10-27 12:03:08 +0200

    animation ANFANG

--- Assets/Scripts/AnimationPlayer.cs
+++ Assets/Scripts/AnimationPlayer.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Animations;
 public class AnimationPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] Animator animator;
+    [SerializeField] Camera cam;
+    Vector2 mousePosition;
+    
     void Update()
     {
         
+
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 mouseVector = mousePosition - new Vector2(transform.position.x, transform.position.y);
+
+        mouseVector = mouseVector.normalized;
+
+        animator.SetFloat("Horizontal", mouseVector.x);
+
+        animator.SetFloat("Vertical", mouseVector.y);
+
+        Debug.Log(mouseVector);
+        /* Vector2 zweidpoisition = new Vector2 (transform.position.x, transform.position.y);
+
+        Vector2 lookdir = mousePosition - zweidpoisition;
+
+        float angle = Mathf.Atan2(lookdir.y, lookdir.x)* Mathf.Rad2Deg - 90;
+
+        transform.eulerAngles = new Vector3(0, 0, angle);
+        */
     }
+
 }
